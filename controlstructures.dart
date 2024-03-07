bba9e1ca0767caa64979d46076f882cd6900630d
Gideonkipyegon <gideonkipyegon837@gmail.com>
2024-03-07 11:40:09 +0300

    first commit

---
+++ controlstructures.dart
@@ -0,0 +1,10 @@
+// writing function outside main function.
+void printName() {
+  print("My name John James");
+  print('and i am 29 years');
+}
+
+// this is our main function.
+void main() {
+  printName();
+}
